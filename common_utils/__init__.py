5441ae2be43ec74d333e27f6f6ca2db88b6fc824
namenaro <nanenaro@gmail.com>
2023-04-14 19:01:05 +0300

    Initial commit

---
+++ common_utils/__init__.py
@@ -0,0 +1,7 @@
+from .ids_generator import IdsGenerator
+from .html_logger import HtmlLogger
+from .point import Point
+from .distrib import Distr
+from .clicker import CoordSelector
+from .pic import Pic
+
